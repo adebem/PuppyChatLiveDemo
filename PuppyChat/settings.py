8159d7fe3ca2c1463ac7c8c6a6d4cdd047a64887
adebem <57114284+adebem@users.noreply.github.com>
2021-10-05 23:29:18 -0700

    Update settings.py

--- PuppyChat/settings.py
+++ PuppyChat/settings.py
@@ -129,10 +129,6 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
-
-#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
-
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
